83b4dabdc3fcf767923242ee6fc8bf9c493051bf
Jamie Bunch <JamieBunch@ymail.com>
2022-02-23 10:32:08 -0500

    added restaurant list

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,8 +1,26 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View } from "react-native";
 
 function RestaurantList() {
-    return <Text>RestaurantList</Text>
+    const [restaurants, setRestaurants] = useState();
+    useEffect(() => {
+        // fetch data from API
+        fetch('https://bocacode-intranet-api.web.app/restaurants')
+        .then(response => response.json())
+        .then(data => setRestaurants(data))
+        .catch(alert);
+    }, []);
+    return (
+    <View>
+    <Text>RestaurantList</Text>
+    {!restaurants
+    ? <Text> Loading... </Text>
+    : restaurants.map(restaurant => {
+        return <Text>{restaurant.name}</Text>
+    })
+    }
+    </View>
+    )
 }
 
 export default RestaurantList;
