6433fe2420987621f6757eae3f4756b0aa062df4
Jamie Bunch <JamieBunch@ymail.com>
2022-02-23 13:01:32 -0500

    updated code for slide menu

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { Text, View, ScrollView } from 'react-native';
+import RestaurantCard from '../components/RestaurantCard';
 
 function RestaurantList() {
-    const [restaurants, setRestaurants] = useState();
-    useEffect(() => {
-        // fetch data from API
-        fetch('https://bocacode-intranet-api.web.app/restaurants')
-        .then(response => response.json())
-        .then(data => setRestaurants(data))
-        .catch(alert);
-    }, []);
-    return (
+  const [restaurants, setRestaurants] = useState();
+  useEffect(() => {
+    // fetch data from API
+    fetch('https://bocacode-intranet-api.web.app/restaurants')
+      .then(response => response.json())
+      .then(data => setRestaurants(data))
+      .catch(alert);
+  }, []);
+  return (
     <View>
-    <Text>RestaurantList</Text>
-    {!restaurants
-    ? <Text> Loading... </Text>
-    : restaurants.map(restaurant => {
-        return <Text>{restaurant.name}</Text>
-    })
-    }
+      {!restaurants
+        ? <Text>Loading...</Text>
+        : <ScrollView>{restaurants.map(restaurant => {
+            return <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+        })}</ScrollView>
+      }
     </View>
-    )
+  );
 }
 
 export default RestaurantList;
